2fee8ee4d2e65149691b0a6f99275190f66417ca
Mehdy <contact@mehdy-elm.com>
2022-10-17 14:48:42 +0200

    adding vendor because no acf-pro

---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'wct/wct',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'composer/installers' => array(
+            'pretty_version' => 'v2.2.0',
+            'version' => '2.2.0.0',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/./installers',
+            'aliases' => array(),
+            'reference' => 'c29dc4b93137acb82734f672c37e029dfbd95b35',
+            'dev_requirement' => false,
+        ),
+        'wct/wct' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'wpackagist-plugin/advanced-custom-fields' => array(
+            'pretty_version' => '5.12.3',
+            'version' => '5.12.3.0',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../advanced-custom-fields',
+            'aliases' => array(),
+            'reference' => 'tags/5.12.3',
+            'dev_requirement' => false,
+        ),
+    ),
+);
